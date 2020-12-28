d010533cb7f566d9c3624d0a9f52050af75549f3
Viktoryia <vik.nik.09.11.1995@gmail.com>
2020-12-28 22:47:46 +0300

    modified BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,24 +1,33 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import lombok.extern.log4j.Log4j2;
+import org.openqa.selenium.SessionNotCreatedException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import pages.BoardPage;
 import pages.EnterPage;
 import pages.LoginPage;
 import pages.MainPage;
+import utils.CapabilitiesGenerator;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-
+@Log4j2
+@Listeners(TestListener.class)
 public class BaseTest {
 
     LoginPage loginPage;
     MainPage mainPage;
     BoardPage boardPage;
     EnterPage enterPage;
-
+    WebDriver driver;
     @BeforeMethod
-    public void setup() {
+    public void setup(ITestContext context) {
 
         Configuration.browser = "chrome";
         Configuration.startMaximized = true;
@@ -29,6 +38,16 @@ public class BaseTest {
         boardPage = new BoardPage();
         enterPage = new EnterPage();
 
+        try {
+            driver = new ChromeDriver(CapabilitiesGenerator.getChromeOptions());
+        } catch (SessionNotCreatedException ex) {
+            Assert.fail("Браузер не был открыт. Проверьте, что используется корректная версия драйвера");
+            log.fatal(ex.getLocalizedMessage());
+        }
+
+        String variable = "driver";
+        System.out.println("Setting driver into context with variable name " + variable);
+        context.setAttribute(variable, driver);
     }
 
 
