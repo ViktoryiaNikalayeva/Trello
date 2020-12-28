092b8b28d9327ffb45ddfd1cd37c3db23ee5af01
Viktoryia <vik.nik.09.11.1995@gmail.com>
2020-12-28 23:26:32 +0300

    add steps

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,8 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import pages.BoardPage;
 import pages.EnterPage;
 import pages.LoginPage;
@@ -10,6 +12,8 @@ import pages.MainPage;
 
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
+@Listeners(TestListener.class)
+@Log4j2
 public class BaseTest {
 
     LoginPage loginPage;
@@ -17,7 +21,7 @@ public class BaseTest {
     BoardPage boardPage;
     EnterPage enterPage;
 
-    @BeforeMethod
+    @BeforeMethod(description = "configuration and initialization")
     public void setup() {
 
         Configuration.browser = "chrome";
@@ -32,10 +36,13 @@ public class BaseTest {
     }
 
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true, description = "driver quit")
     public void tearDown() {
-        {
+        try {
             getWebDriver().quit();
+        } catch (IllegalStateException ex) {
+            log.warn("WebDriver is not opened on attempt to close it");
+            log.warn(ex.getLocalizedMessage());
         }
 
     }
