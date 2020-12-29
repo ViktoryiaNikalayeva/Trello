11cd47978317b4455e03340faded35c80a8c8c51
Viktoryia <vik.nik.09.11.1995@gmail.com>
2020-12-29 16:20:00 +0300

    add other

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -24,7 +24,7 @@ public class BaseTest {
     @BeforeMethod(description = "configuration and initialization")
     public void setup() {
 
-        Configuration.browser = "opera";
+        Configuration.browser = "chrome";
         Configuration.startMaximized = true;
         Configuration.timeout = 30000;
         Configuration.headless = false;
